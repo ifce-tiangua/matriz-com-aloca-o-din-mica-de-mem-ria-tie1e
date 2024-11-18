f8eace83debbbba45e1e9419fd4bfb8447ef2d3b
tie1e <tielemunizalves@gmail.com>
2024-11-18 14:09:03 -0300

    Teste_1

--- main.c
+++ main.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
+int main() {
+    int linhas, colunas;
+    int **matriz;
+    
+    scanf("%d", &linhas);
+    scanf("%d", &colunas);
+    
+    matriz = (int **)malloc(linhas * sizeof(int *));
+    
+    for (int i = 0; i < linhas; i++) {
+        matriz[i] = (int *)malloc(colunas * sizeof(int));
+    }
+    
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+    
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+    
+    for (int i = 0; i < linhas; i++) {
+        free(matriz[i]);
+    }
+    free(matriz);
 
     return 0;
 }
