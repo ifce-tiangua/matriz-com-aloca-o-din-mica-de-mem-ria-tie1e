a891ca0b0bcb662329bd12055b9bac8177b2c4c1
tie1e <tielemunizalves@gmail.com>
2024-11-21 12:21:46 -0300

    Teste_3

--- main.c
+++ main.c
@@ -4,33 +4,33 @@
 int main() {
     int linhas, colunas;
     int **matriz;
-    
+
     scanf("%d", &linhas);
     scanf("%d", &colunas);
-    
+
     matriz = (int **)malloc(linhas * sizeof(int *));
-    
+
     for (int i = 0; i < linhas; i++) {
         matriz[i] = (int *)malloc(colunas * sizeof(int));
     }
-    
+
     for (int i = 0; i < linhas; i++) {
         for (int j = 0; j < colunas; j++) {
             scanf("%d", &matriz[i][j]);
         }
     }
 
-    if (matriz == 0){
+    if (colunas == 0 || linhas == 0){
         printf("[matriz vazia]");
     }
-    
+
     for (int i = 0; i < linhas; i++) {
         for (int j = 0; j < colunas; j++) {
             printf("%d ", matriz[i][j]);
         }
         printf("\n");
     }
-    
+
     for (int i = 0; i < linhas; i++) {
         free(matriz[i]);
     }
